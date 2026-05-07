4c38f34a07c1462f48a1eab30e56b9d49eb8495f
jpbusti <jpbustillo@uninorte.edu.co>
2026-05-06 20:13:48 -0500

    Iconos del menu implementados
    Falta la musica y el sonido

--- proyecto-feria-iii/scripts/ui/main_menu.gd
+++ proyecto-feria-iii/scripts/ui/main_menu.gd
@@ -2,41 +2,130 @@ extends Control
 
 @onready var hover_sfx = $sfx_hover
 @onready var click_sfx = $sfx_click
+@onready var music = $music
+@onready var animation_player = $AnimationPlayer
+
+@onready var btn_multiplayer = $btn_multiplayer
+@onready var btn_options = $btn_options
+@onready var btn_credits = $btn_credits
+@onready var btn_exit = $btn_exit
+
 
 func _ready():
-	_connect_buttons()
-	_play_intro_animation()
 
-func _connect_buttons():
-	for button in $menu_container.get_children():
-		button.mouse_entered.connect(_on_hover.bind(button))
-		button.pressed.connect(_on_click.bind(button))
+	if music:
+		music.play()
 
-func _on_hover(button):
-	hover_sfx.play()
-	
-	# efecto escala tipo Persona
+	if animation_player:
+		animation_player.play("intro")
+
+	_connect_button(btn_multiplayer)
+	_connect_button(btn_options)
+	_connect_button(btn_credits)
+	_connect_button(btn_exit)
+
+	btn_multiplayer.pressed.connect(_on_multiplayer_pressed)
+	btn_options.pressed.connect(_on_options_pressed)
+	btn_credits.pressed.connect(_on_credits_pressed)
+	btn_exit.pressed.connect(_on_exit_pressed)
+
+
+
+func _connect_button(button):
+
+	# CENTRO ESCALADO
+	button.pivot_offset = button.size / 2
+
+	# HOVER
+	button.mouse_entered.connect(_on_button_hover.bind(button))
+	button.mouse_exited.connect(_on_button_exit.bind(button))
+
+func _on_button_hover(button):
+
+	# SONIDO
+	if hover_sfx:
+		hover_sfx.play()
+
+	# TWEEN
 	var tween = create_tween()
-	tween.tween_property(button, "scale", Vector2(1.1,1.1), 0.1)
 
-func _on_click(button):
-	click_sfx.play()
+	tween.set_trans(Tween.TRANS_BACK)
+	tween.set_ease(Tween.EASE_OUT)
 
-	match button.name:
-		"btn_multiplayer":
-			_change_scene("res://scenes/menus/lobby.tscn")
-		"btn_options":
-			_change_scene("res://scenes/menus/options_menu.tscn")
-		"btn_help":
-			_change_scene("res://scenes/menus/help_menu.tscn")
-		"btn_exit":
-			get_tree().quit()
+	# ESCALA
+	tween.tween_property(
+		button,
+		"scale",
+		Vector2(1.05, 1.05),
+		0.08
+	)
 
-func _change_scene(path):
+	# LABEL
+	var label = button.get_child(0)
+
+	if label:
+		label.modulate = Color(1, 0.2, 0.2)
+
+
+
+func _on_button_exit(button):
+
 	var tween = create_tween()
-	tween.tween_property(self, "modulate:a", 0.0, 0.3)
-	await tween.finished
-	get_tree().change_scene_to_file(path)
 
-func _play_intro_animation():
-	$AnimationPlayer.play("intro")
+	tween.set_trans(Tween.TRANS_BACK)
+	tween.set_ease(Tween.EASE_OUT)
+
+	# VOLVER NORMAL
+	tween.tween_property(
+		button,
+		"scale",
+		Vector2.ONE,
+		0.08
+	)
+
+	# LABEL BLANCO
+	var label = button.get_child(0)
+
+	if label:
+		label.modulate = Color.WHITE
+
+func _on_multiplayer_pressed():
+
+	_play_click()
+
+	get_tree().change_scene_to_file(
+		"res://scenes/menu/lobby.tscn"
+	)
+
+
+
+func _on_options_pressed():
+
+	_play_click()
+
+	get_tree().change_scene_to_file(
+		"res://scenes/menu/options_menu.tscn"
+	)
+
+
+
+func _on_credits_pressed():
+
+	_play_click()
+
+	get_tree().change_scene_to_file(
+		"res://scenes/menu/credits_menu.tscn"
+	)
+
+
+
+func _on_exit_pressed():
+
+	_play_click()
+
+	get_tree().quit()
+
+func _play_click():
+
+	if click_sfx:
+		click_sfx.play()
