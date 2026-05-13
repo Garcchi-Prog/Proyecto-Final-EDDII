04fb2ad21e7b579c936cf4183bc5fc7e5cde5a14
Ismael <inobles@uninorte.edu.co>
2026-05-13 01:24:06 -0500

    todas las pruebas

---
+++ proyecto-feria-iii/scenes/detective/lab/papelera.gd
@@ -0,0 +1,11 @@
+extends TextureRect
+
+# Variable para rastrear si ya se hizo clic
+var ya_clicado: bool = false
+
+func _gui_input(event):
+	# Verificamos si es un clic izquierdo y si NO ha sido clicado antes
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		if not ya_clicado:
+			print("¡Encontraste el registro de asistencia de esta clase!\n¿Porque estaba aquí?")
+			ya_clicado = true # Cambiamos el estado para bloquear futuros mensajes
