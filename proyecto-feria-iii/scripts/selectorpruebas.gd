cac90f750fc4b0c6f26a5029198bf6bb553332dc
Garcchi-Prog <gonzalezan@uninorte.edu.co>
2026-05-23 13:50:33 -0500

    Proceso Inventario Pruebas

---
+++ proyecto-feria-iii/scripts/selectorpruebas.gd
@@ -0,0 +1,27 @@
+extends CanvasLayer
+
+@onready var grid = $Panel/GridContainer
+
+func _ready():
+
+	for prueba in GameManager.pruebas:
+
+		var boton = Button.new()
+
+		boton.text = prueba[0]
+
+		boton.custom_minimum_size = Vector2(200, 80)
+
+		boton.pressed.connect(
+			func():
+				seleccionar_prueba(prueba[0])
+		)
+
+		grid.add_child(boton)
+
+
+func seleccionar_prueba(nombre):
+
+	GameManager.prueba_seleccionada = nombre
+
+	queue_free()
